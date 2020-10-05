835743ba96d01acf9f451af61ab130f0a032bf25
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2020-10-05 12:13:12 -0300

    Add Procfile

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,17 +2,32 @@ import Fluent
 import FluentPostgresDriver
 import Vapor
 
+extension Environment {
+    
+    static var databaseURL: URL? {
+        if let urlString = Environment.get("DATABASE_URL"), let url = URL(string: urlString) {
+            return url
+        }
+        return nil
+    }
+}
+
+
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        username: Environment.get("DATABASE_USERNAME") ?? "vinicius",
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? "papalavrasdb"
-    ), as: .psql)
+    if let databaseUrl = Environment.databaseURL {
+        try app.databases.use(.postgres(url: databaseUrl), as: .psql)
+    } else {
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            username: Environment.get("DATABASE_USERNAME") ?? "vinicius",
+            password: Environment.get("DATABASE_PASSWORD") ?? "",
+            database: Environment.get("DATABASE_NAME") ?? "papalavrasdb"
+        ), as: .psql)
+    }
 
     app.migrations.add(CreateUser())
     app.migrations.add(CreateToken())
